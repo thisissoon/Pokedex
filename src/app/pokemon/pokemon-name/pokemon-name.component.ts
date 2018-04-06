0e351c7ffd16215c546855685039a0af3b93a66e
Jack Matthews <jackhkmatthews@gmail.com>
2018-04-06 16:33:57 +0100

    feat(App structure): generate and template for pokemon name component

---
+++ src/app/pokemon/pokemon-name/pokemon-name.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-name',
+  templateUrl: './pokemon-name.component.html',
+  styleUrls: ['./pokemon-name.component.scss']
+})
+export class PokemonNameComponent implements OnInit {
+
+  @Input()
+  public name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
